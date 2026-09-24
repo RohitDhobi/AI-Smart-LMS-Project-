13cf13c8c12f90144f1f329e742282308b9b5d95
RohitDhobi <rohitdhobi@users.noreply.github.com>
2026-09-24 19:11:44 +0530

    Auto-commit: 24-09-2026 19:11:43.94 (1 file changed, 1 insertion(+))

--- backend/src/main/java/com/aismartlms/backend/controller/HODController.java
+++ backend/src/main/java/com/aismartlms/backend/controller/HODController.java
@@ -4,6 +4,7 @@ import com.aismartlms.backend.dto.HODAssignmentView;
 import com.aismartlms.backend.dto.HODDashboardView;
 import com.aismartlms.backend.dto.HODRequest;
 import com.aismartlms.backend.entity.Role;
+import com.aismartlms.backend.entity.User;
 import com.aismartlms.backend.exception.AccessDeniedException;
 import com.aismartlms.backend.repository.InstructorCourseAssignmentRepository;
 import com.aismartlms.backend.repository.UserRepository;
