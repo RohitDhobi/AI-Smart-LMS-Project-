2ad2faacfa7425d8dd76543ba007edbb66f5c549
RohitDhobi <rohitdhobi@users.noreply.github.com>
2026-09-24 21:44:02 +0530

    Auto-commit: 24-09-2026 21:44:02.67 (1 file changed, 4 insertions(+), 1 deletion(-))

--- src/App.jsx
+++ src/App.jsx
@@ -538,7 +538,10 @@ function App() {
         <Route path="settings" element={<AdminSettings />} />
         <Route path="audit-logs" element={<AdminAuditLogs />} />
       </Route>
-      <Route path="/subject/:id" element={<Protected><StudentLayout />}</Protected>>
+      <Route
+        path="/subject/:id"
+        element={<Protected><StudentLayout />}</Protected>
+      >
         <Route index element={<SubjectLearning />} />
       </Route>
       <Route
