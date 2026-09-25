a9cee0ec4dd824c136e36ae28ca431502e0230f9
RohitDhobi <rohitdhobi@users.noreply.github.com>
2026-09-25 18:37:11 +0530

    Auto-commit: 25-09-2026 18:37:11.62 (1 file changed, 1 insertion(+))

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
+import "./hod.css";
 
 // Error boundary: if the app ever crashes during render,
 // show a readable message instead of a blank page.
